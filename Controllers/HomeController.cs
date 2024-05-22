a0fc564f1dfa36fc0a645d6cb8c87b0bc0019eae
kobzevastep@gmail.com <kobzevastep@gmail.com>
2024-05-22 18:53:41 +0300

    Added Website Visual

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FilmPage(int index)
+        {
+            return View();
+        }
+    }
+}
